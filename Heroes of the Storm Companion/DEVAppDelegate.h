f1fd191a1b1ea658673bef34d3f3a420344df27c
Devin Rider <devinrrider@gmail.com>
2014-08-19 10:33:57 -0700

    Initial Commit

---
+++ Heroes of the Storm Companion/DEVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEVAppDelegate.h
+//  Heroes of the Storm Companion
+//
+//  Created by Devin on 8/19/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Heroes of the Storm CompanionTests/Heroes_of_the_Storm_CompanionTests.m
@@ -0,0 +1,34 @@
+//
+//  Heroes_of_the_Storm_CompanionTests.m
+//  Heroes of the Storm CompanionTests
+//
+//  Created by Devin on 8/19/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Heroes_of_the_Storm_CompanionTests : XCTestCase
+
+@end
+
+@implementation Heroes_of_the_Storm_CompanionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
